711a428c598d104e957056c80105eabf71f01369
blbi <sskk1108@naver.com>
2017-07-31 12:33:24 -0400

    finalgit add myarp.c

--- ARP/myarp.c
+++ ARP/myarp.c
@@ -11,40 +11,41 @@
 #include <arpa/inet.h>
 #include <netinet/if_ether.h>
 #include <net/ethernet.h>
-#include <net/if_arp.h>
+
 
 #define strerr 2
+#define ETHER_ARP 0x0806
 
-
-/*
 typedef struct arp{
     unsigned short htype;
     unsigned short ptype;
     char hs;
     char ps;
     unsigned short opc;
-    u_char smac[6];
-    u_char sip[4];
-    u_char tmac[6];
-    u_char tip[4];
+    unsigned char smac[6];
+    unsigned char sip[4];
+    unsigned char tmac[6];
+    unsigned char tip[4];
     
 
 }ARP;   
 
-typedef struct eth{
+/*typedef struct eth{
     u_char dest_addr[6];
     u_char src_addr[6];
     u_short type;
     ARP arph;
-}ETH;
-*/
+}ETH;*/
 
 int main(int argc, char *argv[]){
 //int main(char interface[], char sender_ip[], char target_ip[]){
 	
 	char *interface=argv[1];
-	char *sender_ip=argv[2];
-	char *target_ip = argv[3];
+	
+	struct sockaddr_in sender_ip;
+	struct sockaddr_in target_ip;
+//char *sender_ip=argv[2];
+	//char *target_ip = argv[3];
 
 	pcap_t *pcd;
 	
@@ -53,9 +54,10 @@ int main(int argc, char *argv[]){
 
 	int fd;
 	struct ifreq ifr;
-	struct ether_header ethh;
+	struct ether_header *ETH;
+	
 	u_char *packet=malloc(42);
-	//ethh = (struct ether_header *)(packet);
+	ETH = (struct ether_header*)(packet);
 	
 
 //=====================get my address====================
@@ -75,9 +77,9 @@ int main(int argc, char *argv[]){
 //=================get mac============
 	ioctl(fd, SIOCGIFHWADDR, &ifr); //mac address
 
-	//char *my_mac= (unsigned char*)ifr.ifr_hwaddr.sa_data;
 	u_int8_t my_mac[ETH_ALEN];
 	memcpy(my_mac, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
+
 	
 
 /*
@@ -86,7 +88,7 @@ int main(int argc, char *argv[]){
 	int i,j;
 	for(i=0, j=0; i < 6 ; i++, j+=2)
 	{ 
-		sprintf((char*)my_mac + j, "%02X", ((unsigned char*)ifr.ifr_hwaddr.sa_data)[i]);
+     	sprintf((char*)my_mac + j, "%02X", ((unsigned char*)ifr.ifr_hwaddr.sa_data)[i]);
 	}
 */
 	close(fd);
@@ -107,8 +109,8 @@ int main(int argc, char *argv[]){
 //====================================================
 
 //	ethh->dest_addr = malloc
-	memcpy(ethh.ether_dhost,"\xff\xff\xff\xff\xff\xff",6);
-	memcpy(ethh.ether_shost,my_mac,6);
+	memcpy(ETH->ether_dhost,"\xff\xff\xff\xff\xff\xff",6);
+	memcpy(ETH->ether_shost,my_mac,6);
 
 //	ethh->dest_addr = "\xff\xff\xff\xff\xff\xff";				
 //	ethh->src_addr = my_mac;
@@ -116,39 +118,30 @@ int main(int argc, char *argv[]){
 printf("%s", my_mac);
 
 	char myipaddr[INET_ADDRSTRLEN];
+	u_int8_t mac[ETH_ALEN];
+	u_int8_t broadcast[ETH_ALEN] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+	struct ether_arp arph;
 
-	inet_ntop(AF_INET, &(my_ip.s_addr), myipaddr, INET_ADDRSTRLEN);
-
-
-	ethh.ether_type=htons(ETHERTYPE_ARP);
-
-//==================================================
-	struct ether_arp arp;
-
-	arp.arp_hrd = htons(0x0001);
-
-	arp.arp_pro = htons(0x0800);
-	arp.arp_hln = 0x06;
-	arp.arp_pln = 0x04;
-	arp.arp_op = 0x0100;
-	memcpy(arp.arp_sha,my_mac,6);
-	memcpy(arp.arp_spa,myipaddr,4);
-	memcpy(arp.arp_tha,"\x00\x00\x00\x00\x00\x00",6);
-	memcpy(arp.arp_tpa,sender_ip,4);
-
-
-
-//	ethh->arph.smac = my_mac;
-//	ethh->arph.sip = my_ip;
-//	ethh->arph.tip = send_ip;
-
-//	*packet = (char *)malloc(sizeof(ethh));
-//	memcpy(packet,(void *)&ethh,sizeof(ethh));
-
-
-	memcpy(packet, &ethh,sizeof(ethh));
-	memcpy(packet+14, &arp, sizeof(arp));
+	ETH->ether_type = ntohs(ETHER_ARP);
 	
+	inet_aton(argv[2], &sender_ip.sin_addr); inet_aton(argv[3], &target_ip.sin_addr);
+
+
+
+	arph.arp_hrd = htons(ARPHRD_ETHER);
+	arph.arp_pro = htons(ETH_P_IP);
+	arph.arp_hln = 0x06;
+	arph.arp_pln = 0x04;
+	arph.arp_op = htons(ARPOP_REQUEST);
+	memcpy(arph.arp_sha, my_mac, ETH_ALEN);
+	memcpy(arph.arp_spa, &sender_ip.sin_addr, 4);
+	memcpy(arph.arp_tha, broadcast, ETH_ALEN);
+	memcpy(arph.arp_tpa, &target_ip.sin_addr, 4);
+ 
+	memcpy(packet,ETH,sizeof(ETH));
+	memcpy(packet+14, &arph, sizeof(struct ether_arp));
+
+
 
 	pcap_sendpacket(pcd, packet, 42);
 //=====================================================
